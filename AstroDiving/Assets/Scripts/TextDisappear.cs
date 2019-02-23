5b9ff57bd6ec41c4d2eed8377ae7e52206e490c0
Tiisetso <tiisetsomurray@gmail.com>
2019-02-23 14:38:41 +0200

    Disappearing tutorial text.

---
+++ AstroDiving/Assets/Scripts/TextDisappear.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TextDisappear : MonoBehaviour {
+      public float time = 5f; 
+      
+      void Start()
+      {
+          Invoke("Disappear", time);
+      }
+ 
+      void Disappear(){
+          Destroy(gameObject);
+      }
+      
+ }
